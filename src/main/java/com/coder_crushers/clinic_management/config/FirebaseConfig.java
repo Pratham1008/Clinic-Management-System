8c703b718e26c2f29ebb322b9ac0c05df8d663cc
Prathamesh <prathamesh10082004@gmail.com>
2025-04-23 00:46:35 +0530

    service_key

--- src/main/java/com/coder_crushers/clinic_management/config/FirebaseConfig.java
+++ src/main/java/com/coder_crushers/clinic_management/config/FirebaseConfig.java
@@ -17,7 +17,7 @@ public class FirebaseConfig {
     public FirebaseAuth initialize() {
         try {
             FileInputStream serviceAccount =
-                    new FileInputStream("src/main/resources/serviceKey.json");
+                    new FileInputStream("app/src/main/resources/serviceKey.json");
 
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
